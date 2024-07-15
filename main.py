5b6e6af884b6cc7933823cc2c269b03793829ec4
vivek kasarla <vivekkasarla781@gmail.com>
2024-07-15 15:47:43 +0000

    listOperations

---
+++ main.py
@@ -0,0 +1,12 @@
+# program to perfrom basic operations on list:
+new_list=[1,2,3,4,5]
+# adding elements into the list
+new_list.append(6)
+# adding elements at a specific positon
+new_list.insert(0,0)
+# removing elements from the list
+new_list.remove(2)
+# removing elements from a specific position
+new_list.pop(3)
+
+print("updated list after operations:",new_list)
